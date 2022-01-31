7a4e5fec2de258bb9fe6da8df1a468be72b1777e
jasper1196 <jasper1196@outlook.com>
2022-01-31 22:44:32 +0100

    Set up useForm for register form. Also done the validations on the formfields.

--- src/pages/registerPage/RegisterPage.js
+++ src/pages/registerPage/RegisterPage.js
@@ -1,8 +1,130 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {useForm} from "react-hook-form";
+import "./RegisterPage.css";
 
 function RegisterPage() {
+    const {register, handleSubmit, formState: {errors}, watch} = useForm();
+
+    function onFormSubmit(regData) {
+        console.log(regData);
+    }
+
+    const passwordWatcher = watch("password", "");
+    const confirmationWatcher = watch("confirmation", "")
+
+    function validateNumber(value) {
+        return (/\d/.test(value));
+    }
+
+    function validateLowercase(value) {
+        return (/[a-z]/.test(value));
+    }
+
+    function validateUppercase(value) {
+        return (/[A-Z]/.test(value));
+    }
+
+    function validateSpecial(value) {
+        return (/[!@#$%^&*()-.,+]/.test(value));
+    }
+
+    function validateConfirmation(value, confirmationValue) {
+        return (value === confirmationValue && confirmationValue !== "");
+    }
+
     return (
-        <label>register</label>
+        <div className="register-container">
+            <label>Registreren</label>
+            <form onSubmit={handleSubmit(onFormSubmit)}>
+                <div>
+                    <label htmlFor="username-reg">Gebruikersnaam</label>
+                    <input
+                        id="username-reg"
+                        type="text"
+                        {...register("username", {
+                            required: "De gebruikersnaam moet ingevuld zijn.",
+                            minLength: {
+                                value: 6,
+                                message: "De gebruikersnaam moet minimaal 6 karakters bevatten."
+                            },
+                            maxLength: {
+                                value: 30,
+                                message: "De gebruikersnaam mag maximaal 30 karakters bevatten."
+                            }
+                        })}
+                    />
+                    {errors.username && <p className="error-msg">{errors.username.message}</p>}
+                </div>
+                <div>
+                    <label htmlFor="email-reg">Email</label>
+                    <input
+                        id="email-reg"
+                        type="text"
+                        {...register("email", {
+                            required: "Het emailadres moet ingevuld zijn.",
+                            pattern:{
+                                value: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+                                message: "Dit is geen geldig emailadres."
+                            }
+                        })}
+                    />
+                    {errors.email && <p className="error-msg">{errors.email.message}</p>}
+                </div>
+                <div>
+                    <label htmlFor="password-reg">Wachtwoord</label>
+                    <input
+                        id="password-reg"
+                        type="password"
+                        {...register("password",{
+                            required: true,
+                            minLength: 8,
+                            validate: () => {
+                                if (
+                                    validateUppercase(passwordWatcher) &&
+                                    validateLowercase(passwordWatcher) &&
+                                    validateSpecial(passwordWatcher) &&
+                                    validateNumber(passwordWatcher) &&
+                                    validateConfirmation(passwordWatcher, confirmationWatcher)
+                                ) {
+                                    return true;
+                                } else {
+                                    return false;
+                                }
+                            }
+                        })}
+                    />
+                    <div id="password-validation-rules">
+                        <p className={passwordWatcher.length >= 8 ? "requirement-met" : "requirement-not-met"}>
+                            Minimaal 8 karakter
+                        </p>
+                        <p className={validateUppercase(passwordWatcher) ? "requirement-met" : "requirement-not-met"}>
+                            Een hoofdletter
+                        </p>
+                        <p className={validateLowercase(passwordWatcher) ? "requirement-met" : "requirement-not-met"}>
+                            Een kleine letter
+                        </p>
+                        <p className={validateNumber(passwordWatcher) ? "requirement-met" : "requirement-not-met"}>
+                            Een cijfer
+                        </p>
+                        <p className={validateSpecial(passwordWatcher) ? "requirement-met" : "requirement-not-met"}>
+                            Een speciaal teken (!@#$%^&*()-.,+)
+                        </p>
+                        <p className={validateConfirmation(passwordWatcher, confirmationWatcher) ? "requirement-met" : "requirement-not-met"}>
+                            Wachtwoorden komen overeen
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <label htmlFor="password-confirmation">Herhaal Wachtwoord</label>
+                    <input
+                        id="password-confirmation"
+                        type="password"
+                        {...register("confirmation")}
+                    />
+                </div>
+                <button type="submit">Registreren</button>
+            </form>
+        </div>
     );
 }
 
